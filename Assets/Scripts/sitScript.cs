3ce147730b9887e8216d6cc6ba9dcf05a27c4a98
austinshyu <austinshyu@berkeley.edu>
2014-11-09 06:05:57 -0800

    assets

---
+++ Assets/Scripts/sitScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sitScript : MonoBehaviour {
+	//public bool setFalse = true;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnTriggerStay(Collider collide){
+		print ("can sit");
+		if (collide.gameObject.name == "person") {
+			//setFalse = false;
+			collide.gameObject.GetComponent<mainchar>().canSit = true;
+						if (this.gameObject.name == "next_to_creepy") {
+								collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (this.transform.position.x, 1.1f, this.transform.position.z - 0.7f); 
+						}
+						else if(this.gameObject.name == "next_to_madam"){
+				collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (3.9f, 1.1f, this.transform.position.z +0.25f);
+						}
+				}
+	}
+	void OnTriggerExit(Collider collide){
+		if (collide.gameObject.name == "person") {
+			//setFalse = false;
+			/*collide.gameObject.GetComponent<mainchar>().canSit = true;
+			if (this.gameObject.name == "next_to_creepy") {
+				collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.7f); 
+			}*/
+			collide.gameObject.GetComponent<mainchar>().canSit = false;
+		}
+	}
+}
