4abaccd9c810e944017ada03f853322095a8d870
austinshyu <austinshyu@berkeley.edu>
2014-11-09 12:51:04 -0800

    Global.cs initial dictionary made

--- Assets/Scripts/Global.cs
+++ Assets/Scripts/Global.cs
@@ -1,13 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Global : MonoBehaviour {
 	DialogueGUI dialogueGUI;
-
+	public Dictionary<string,string> values = new Dictionary<string,string>();
 	// Use this for initialization
 	void Start () {
 		dialogueGUI = GetComponent<DialogueGUI>();
+		values.Add ("talkedToCreeper", "1");
+		values.Add ("talkedToMadam", "0");
+		values.Add ("talkedToBully", "0");
+		values.Add ("talkedToMother", "0");
+		values.Add ("hideCreeper", "0"); // hide creeper if talked to everyone once
+		values.Add ("tookKnife", "0");
+
 	}
-	
+	void Update(){
+		Debug.Log(ValueOfKey ("talkedToCreeper"));
+	}
+	string ValueOfKey(string key){
+		string rv = "0";
+		values.TryGetValue(key,out rv);
+		return rv; 
+	}
 	public DialogueGUI DialogueGUI { get { return dialogueGUI; } }
 }
