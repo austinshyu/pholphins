434ea2e2b7dc6c0adba60fde9e36ac316e8cd4e2
ChrisLHall <c3doggy@gmail.com>
2014-11-09 15:18:55 -0800

    Added cop and ground, changed stuff to be black.

--- Assets/Scripts/TreeMover.cs
+++ Assets/Scripts/TreeMover.cs
@@ -4,16 +4,21 @@ using System.Collections;
 public class TreeMover : MonoBehaviour {
 	public float wrapWidth;
 	public float xSpeed;
+	GameObject[] grounds = new GameObject[2];
 
 	// Use this for initialization
 	void Start () {
-	
+		grounds[0] = transform.FindChild("Ground1").gameObject;
+		grounds[1] = transform.FindChild("Ground2").gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		Transform[] childTransforms = gameObject.GetComponentsInChildren<Transform>();
 		foreach (Transform t in childTransforms) {
+			if (t.name != "tree") {
+				continue;
+			}
 			Vector3 moved = t.position + new Vector3(xSpeed, 0f, 0f);
 			t.position = moved;
 			Vector3 wrap = new Vector3(2f * wrapWidth, 0f, 0f);
@@ -22,6 +27,12 @@ public class TreeMover : MonoBehaviour {
 			} else if (t.position.x > wrapWidth) {
 				t.position -= wrap;
 			}
+		}
+
+		float shiftTex = xSpeed / grounds[0].transform.lossyScale.x / 10f;
+		foreach (GameObject ground in grounds) {
+			ground.renderer.material.SetTextureOffset("_MainTex",
+	  				ground.renderer.material.GetTextureOffset("_MainTex") + new Vector2(shiftTex, 0f));
 		}
 	}
 }
