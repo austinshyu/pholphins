86adba023659200341f4dc5b8018c09e15ceddb9
ChrisLHall <c3doggy@gmail.com>
2014-11-09 09:06:55 -0800

    Dialog blobs!

--- Assets/Scripts/sitScript.cs
+++ Assets/Scripts/sitScript.cs
@@ -2,29 +2,32 @@
 using System.Collections;
 
 public class sitScript : MonoBehaviour {
+	Vector3 sitLocation;
+	GameObject bubble;
 	//public bool setFalse = true;
 	// Use this for initialization
 	void Start () {
-	
+		sitLocation = transform.parent.FindChild("sit_location").position;
+		bubble = transform.parent.FindChild("talk").gameObject;
+		bubble.SetActive(false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
 	}
+
 	void OnTriggerStay(Collider collide){
-		print ("can sit");
+		print ("can sit" + gameObject.name);
 		if (collide.gameObject.name == "person") {
 			//setFalse = false;
-			collide.gameObject.GetComponent<mainchar>().canSit = true;
-						if (this.gameObject.name == "next_to_creepy") {
-								collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (this.transform.position.x, 1.1f, this.transform.position.z - 0.7f); 
-						}
-						else if(this.gameObject.name == "next_to_madam"){
-				collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (3.9f, 1.1f, this.transform.position.z +0.25f);
-						}
-				}
+			mainchar player = collide.gameObject.GetComponent<mainchar>();
+			player.canSit = true;
+			player.sitLocation = sitLocation;
+			bubble.SetActive(true);
+		}
 	}
+
 	void OnTriggerExit(Collider collide){
 		if (collide.gameObject.name == "person") {
 			//setFalse = false;
@@ -33,6 +36,7 @@ public class sitScript : MonoBehaviour {
 				collide.gameObject.GetComponent<mainchar> ().sitLocation = new Vector3 (this.transform.position.x, this.transform.position.y, this.transform.position.z - 0.7f); 
 			}*/
 			collide.gameObject.GetComponent<mainchar>().canSit = false;
+			bubble.SetActive(false);
 		}
 	}
 }
