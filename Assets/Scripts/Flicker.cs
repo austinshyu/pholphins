0140d5ee22dc02486dad342872e276e5e3080b15
ChrisLHall <c3doggy@gmail.com>
2014-11-09 16:16:15 -0800

    Flickery stuff and nice lights.

---
+++ Assets/Scripts/Flicker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flicker : MonoBehaviour {
+	Light l;
+	float originalIntensity;
+	public float severity;
+	public float probability;
+
+	int counter;
+	// Use this for initialization
+	void Start () {
+		l = GetComponent<Light>();
+		originalIntensity = l.intensity;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 	if (counter <= 0) {
+			l.intensity = originalIntensity;
+			if (Random.Range(0f, 1f) < probability) {
+				counter = 5 + (int) Random.Range(0f, 10f);
+				l.intensity = originalIntensity * (1f - severity);
+			}
+		} else {
+			counter--;
+		}
+	}
+}
