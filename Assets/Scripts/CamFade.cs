bd3176e9b0b6ead00420586608f989b530b5d420
ChrisLHall <c3doggy@gmail.com>
2014-11-09 16:42:19 -0800

    AHHH

---
+++ Assets/Scripts/CamFade.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CamFade : MonoBehaviour {
+
+	public Texture2D fadeTexture;
+	float fadeSpeed = 0.2f;
+	int drawDepth = -1000;
+	
+	private float alpha = 1.0f; 
+	public float fadeDir = -1f;
+	
+	void OnGUI(){
+		
+		alpha += fadeDir * fadeSpeed * Time.deltaTime;  
+		alpha = Mathf.Clamp01(alpha);   
+
+		Color c = GUI.color;
+		c.a = alpha;
+		GUI.color = c;
+		
+		GUI.depth = drawDepth;
+		
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+	}
+}
